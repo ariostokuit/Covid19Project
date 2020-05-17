a5012fa41af7ec86b5228c40bb7d67c338f7a32e
Gabriel Rivera <gabrielrivera457@gmail.com>
2020-05-16 21:41:38 -0700

    forgot change

--- src/pages/page-4.js
+++ src/pages/page-4.js
@@ -15,7 +15,7 @@ const FourthPage = () => {
     return (
       <Layout pageName="four">
         <Helmet>
-          <title>Page Two</title>
+          <title>Tables</title>
           <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" 
       integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" 
       crossorigin="anonymous"></link>
